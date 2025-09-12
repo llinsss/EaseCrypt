02b4a16384981c78eae3ec5fcc1fa4b9681851ff
abdegenius <johnnieabijah2000@gmail.com>
2025-09-12 07:10:59 +0100

    updates

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,8 @@ if (isProduction) {
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
   message: "Too many requests from this IP, please try again later.",
 });
 
